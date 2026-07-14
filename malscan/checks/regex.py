1bb66bd386093011f665301576c0fd367f21a5df
paolo moccia <paolo.moccia@edu-its.it>
2026-07-14 16:13:54 +0200

    inizio regex

--- malscan/checks/regex.py
+++ malscan/checks/regex.py
@@ -1,5 +1,16 @@
 from malscan.engine import Check
 
 class RegexCheck(Check):
+    def __init__(self):
+        self.patterns = {
+            rb"powershell(\.exe)?": "PowerShell Execution Reference",
+            rb"cmd\.exe": "Command Prompt Reference",
+            rb"eval\(|exec\(": "Dynamic Code Execution Pattern",
+            rb"system\s*\(": "System Call Pattern",
+            rb"WScript\.Shell": "Windows Script Host Reference",
+            rb"http[s]?://": "Network Activity Indicator"
+            #da aggiungere altro
+        }
+
     def analyze(self, file_path: str) -> dict:
         pass
