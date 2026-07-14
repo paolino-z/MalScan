f3de4ad1d44f9149f758c8199ee2519baa5f1997
paolo moccia <paolo.moccia@edu-its.it>
2026-07-14 15:40:10 +0200

    aggiunta extension check

--- malscan/checks/extension.py
+++ malscan/checks/extension.py
@@ -1,5 +1,22 @@
+import os
+
 from malscan.engine import Check
 
 class ExtensionCheck(Check):
-    def analyze(self, file_path: str) -> dict:
-        pass
+    def __init__(self):
+        self.sus_extensions = {
+            ".exe", ".dll", ".bat", ".cmd", ".vbs", 
+            ".scr", ".ps1", ".sh", ".msi", ".jar", ".pif"
+        }
+    
+    def analyze(self, file_path):
+        _, ext = os.path.splitext(file_path)
+        is_sus = ext in self.sus_extensions
+        return {
+            "name": "Extension Security Check",
+            "score": 40 if is_sus else 0,
+            "details": {
+                "extension": ext if ext else "None",
+                "is_sus": is_sus
+            }
+        }
